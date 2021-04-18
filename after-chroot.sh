b752e3792fa4c60b871d5e69e322fe1730ef443c
tpenders <tpenders@xs4all.nl>
2021-04-18 10:17:15 +0200

    Update after-chroot.sh

--- after-chroot.sh
+++ after-chroot.sh
@@ -4,15 +4,22 @@ pacman -S nano sudo grub efibootmgr dosfstools os-prober mtools &&
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen &&
 localectl set-locale LANG=en_US.UTF-8 &&
 locale-gen &&
-echo "linux" > /etc/hostname &&                                                                           echo "127.0.0.1 localhost" > /etc/hosts &&                                                                echo "::1 localhost" >> /etc/hosts
-passwd &&                                                                                                 useradd -m arch &&                                                                                        passwd arch &&
+echo "linux" > /etc/hostname &&                                                                           
+echo "127.0.0.1 localhost" > /etc/hosts &&                                                                
+echo "::1 localhost" >> /etc/hosts
+echo "Enter password for root user" &&
+passwd &&                                                                                                 
+useradd -m arch &&
+echo "Enter password for non-root user" &&
+passwd arch &&
 usermod -aG wheel,audio,video,optical,storage arch &&
-mkdir /boot/EFI &&                                                                                        mount /dev/mmcblk0p1 /boot/EFI &&
+mkdir /boot/EFI &&                                                                                        
+mount /dev/mmcblk0p1 /boot/EFI &&
 grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck &&
 grub-mkconfig -o /boot/grub/grub.cfg &&
-pacman -S  networkmanager git xorg xorg-xinit xterm &&
+pacman -S  networkmanager git xorg xorg-xinit &&
 systemctl enable NetworkManager &&
-echo "%wheelALL=(ALL) ALL" >> /etc/sudoers &&
-echo "xterm &" > /home/arch/.xinitrc &&
-echo "exec dwm" >> /home/arch/.xinitrc &&
-echo "Now reboot"
+echo "root ALL=(ALL) ALL" > /etc/sudoers &&
+echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers &&
+echo "@includedir /etc/sudoers.d" >> /etc/sudoers &&
+echo "Done"
