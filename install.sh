c4a637c0cf915e9ab91f582619315a38c609f2ba
tpenders <tpenders@xs4all.nl>
2021-04-18 10:22:49 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 timedatectl set-ntp true &&
 parted /dev/mmcblk0 mklabel gpt &&
-mkpart /dev/mmcblk0 fat32 1MiB 550MiB  &&
-parted /dev/mmcblk0 linux-swap 550MiB 2598MiB &&
-parted /dev/mmcblk0 ext4 2598MiB 100% &&
+parted /dev/mmcblk0 mkpart fat32 1MiB 550MiB  &&
+parted /dev/mmcblk0 mkpart linux-swap 550MiB 2598MiB &&
+parted /dev/mmcblk0 mkpart ext4 2598MiB 100% &&
 mkfs.fat -F32 /dev/mmcblk0p1 &&
 mkswap /dev/mmcblk0p2 &&
 swapon /dev/mmcblk0p2 &&
